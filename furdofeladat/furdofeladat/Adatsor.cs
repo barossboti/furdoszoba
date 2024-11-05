0ab77921a3d6a0937692655122ec5ebfc9d5dc2b
barossboti <150018760+barossboti@users.noreply.github.com>
2024-11-05 13:46:03 +0100

    Add files via upload

---
+++ furdofeladat/furdofeladat/Adatsor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace furdofeladat
+{
+    internal class Adatsor
+    {
+        public int vendegazonosito {get; set; }
+        public int furdoazonosito { get; set; }
+        public bool belep { get; set; }
+        public int ora { get; set; }
+        public int perc { get; set; }
+        public int mp {  get; set; }
+        public Adatsor(string sor)
+        {
+            string[] sz = sor.Split(' ');
+            vendegazonosito = int.Parse(sz[0]);
+            furdoazonosito = int.Parse(sz[1]);
+            if(int.Parse(sz[2]) == 0 )
+            {
+                belep = true;
+            }
+            ora = int.Parse(sz[3]);
+            perc = int.Parse(sz[4]);
+            mp = int.Parse(sz[5]);
+        }
+    }
+}
---
+++ furdofeladat/furdofeladat/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace furdofeladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. feladat
+            List<Adatsor> adatok = new List<Adatsor>();
+            string[] fajl = File.ReadAllLines("furdoadat.txt",Encoding.UTF8);
+            for(int i = 0; i < fajl.Length; i++)
+            {
+                adatok.Add(new Adatsor(fajl[i]));
+            }
+            //2. feladat
+            var feladat2 = adatok.Where(x=>x.belep == false && x.furdoazonosito == 0).ToList();
+
+            Console.WriteLine($"2. feladat: Az első vendég {feladat2[0].ora}:{feladat2[0].perc}:{feladat2[0].mp} lépett ki az öltözőből. ");
+            //3. feladat
+            Console.WriteLine("3. feladat");
+            var feladat3 = adatok.GroupBy(x => x.vendegazonosito);
+            foreach (var item in feladat3 ) {
+                Console.WriteLine($"{item.Count()}");
+
+            }
+            //4. feladat
+            Console.WriteLine("4. feladat");
+            var feladat4 = adatok.Where(x => x.belep == false && x.furdoazonosito ==0 ).ToList();
+            foreach (var item in feladat4) { 
+            Console.WriteLine($"A legtöbb időt eltöltő vendég:\t {item.vendegazonosito}");
+            }
+
+            //5. feladat
+
+            //6. feladat
+            Console.WriteLine("6. feladat: szauna.txt");
+            List<string> kimenet = new List<string>();
+
+            var feladat6 = adatok.Where(x => x.belep == false && x.furdoazonosito == 0).ToList();
+            foreach (var item in feladat6)
+            {
+                kimenet.Add($"{feladat6[0].vendegazonosito} {feladat6[0].ora}:{feladat6[0].perc}:{feladat6[0].mp} ");
+            }
+            File.WriteAllLines("szauna.txt",kimenet);
+
+
+            //7. feladat
+
+            Console.WriteLine("7 feladat");
+            var feladat7 = adatok.GroupBy(x => x.furdoazonosito);
+            foreach (var item in feladat7)
+            {
+                Console.WriteLine($"\t{item.Key} {item.Count()}db");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
